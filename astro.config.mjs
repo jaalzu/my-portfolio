f5403e5a209845e6a9e652570752f3a16e10b299
Javier <javieralzuu@gmail.com>
2025-11-25 03:34:19 -0300

    big improves!

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-
+import icon from 'astro-icon'; // <-- IMPORTA LA LIBRERÍA
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(),icon({
+      // ¡Añade esta opción!
+      include: {
+        devicon: ['*'], // Carga TODOS los iconos de la colección 'devicon'
+        // Puedes añadir otras colecciones si las necesitas, por ejemplo:
+        // 'simple-icons': ['*'],
+      }
+    }),],
 });
